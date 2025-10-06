5b94cd71649b11d5ac65ddcbd31a318912689b8c
Asif294 <asifurrahman677294@gmail.com>
2025-10-06 19:49:10 +0600

    first commit

---
+++ users/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class student (models.Model):
+    name=models.CharField(max_length=30)
+    roll =models.IntegerField(primary_key=True)
+    address=models.TextField()
+    father_name =models.TextField()
+
+    def __str__(self):
+        return f"Roll : {self.roll} - {self.name}"

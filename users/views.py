0b639c89182c840aa961caf3865d18b47b9a7f9c
Asif294 <asifurrahman677294@gmail.com>
2025-10-06 22:48:05 +0600

    delect

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
-
+from .import models
 def home(request):
-    return render(request,"home.html")
+    student=models.student.objects.all()
+    return render(request,"home.html",{'data':student})

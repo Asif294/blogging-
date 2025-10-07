03b8e0cb8aa31cdabe35fd162aee73648d165726
Asif294 <asifurrahman677294@gmail.com>
2025-10-07 21:11:06 +0600

    delect button

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path,include
 from .import views 
 urlpatterns = [
-    path('',views.home)
+    path('',views.home,name="homepage"),
+    path('delete/<int:roll>',views.delete_student,name="delect_student")
     
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .import models
 def home(request):
     student=models.student.objects.all()
     return render(request,"home.html",{'data':student})
+
+def delete_student(request,roll):
+    stu=models.student.objects.get(pk=roll).delete()
+    return redirect("homepage")
+

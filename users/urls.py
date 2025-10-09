c04e4ac26088dd4336a9c0802af4a37fc4e66d89
Asif294 <asifurrahman677294@gmail.com>
2025-10-09 08:11:29 +0600

    last commit

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,7 @@ from django.urls import path,include
 from .import views 
 urlpatterns = [
     path('',views.home,name="homepage"),
+    
     path('delete/<int:roll>',views.delete_student,name="delect_student")
     
 ]
